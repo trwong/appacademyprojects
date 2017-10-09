aa287f01cf21f92e6689de04fb68d5ac5e03f7b8
Taylor Wong <trwong93@gmail.com>
2017-10-09 13:41:58 -0700

    Complete error_handling_funtime

--- w2d1/error_handling_funtime/super_useful.rb
+++ w2d1/error_handling_funtime/super_useful.rb
@@ -1,6 +1,10 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str)
+  rescue ArgumentError
+    return nil
+  end
 end
 
 # PHASE 3
@@ -9,22 +13,32 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  else
+    raise StandardError
+  end
 end
 
 def feed_me_a_fruit
+  begin
   puts "Hello, I am a friendly monster. :)"
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+    reaction(maybe_fruit)
+  rescue StandardError
+    if maybe_fruit == "coffee"
+      retry
+    else
+      puts "Thats not a fruit!"
+    end
+  end
+end
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError if yrs_known < 5
+    raise ArgumentError if name.length == 0 || fav_pastime.length == 0
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
@@ -39,8 +53,6 @@ class BestFriend
   end
 
   def give_friendship_bracelet
-    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
   end
 end
-
-
